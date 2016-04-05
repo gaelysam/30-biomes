d95ae22814eb15dc4e51baacabd5c6655909795c
Gael-de-Sailly <gael.chretien@akeonet.com>
2016-04-05 14:30:18 +0200

    Use a CSV file for biome data. Future modifications should be done using Calc, Excel or any other spreadsheet software. I've chosen this format because it's a text format that is easily readable.

--- init.lua
+++ init.lua
@@ -19,16 +19,56 @@ minetest.register_node("biomes:mud", {
 	sounds = default.node_sound_dirt_defaults(),
 })
 
-dofile(path .. "/main_biomes.lua")
-dofile(path .. "/beach_biomes.lua")
-dofile(path .. "/sea_biomes.lua")
-dofile(path .. "/decorations.lua")
+for line in io.lines(path .. "/biomes.csv") do
+	local attribs = line:split(",", true)
+	local name, stone, fill, top, dust, sea, river, ymin, ymax, heat, humidity = unpack(attribs)
 
-minetest.register_biome({
-	name = "underground",
-	node_stone = "default:stone",
-	y_min = -113,
-	y_max = -31000,
-	heat_point = 50,
-	humidity_point = 50,
-})
+	if #name > 0 and name:sub(1,1) ~= "#" then
+		-- Register biome
+		local biome = {name=name}
+
+		if stone and #stone > 0 then
+			biome.stone = stone
+		end
+
+		if fill and #fill > 0 then
+			fill = fill:split("%s", false, 1, true)
+			biome.node_filler = fill[1]
+			biome.depth_filler = tonumber(fill[2] or 1)
+		end
+
+		if top and #top > 0 then
+			top = top:split("%s", false, 1, true)
+			biome.node_top = top[1]
+			biome.depth_top = tonumber(top[2] or 1)
+		end
+
+		if dust and #dust > 0 then
+			biome.node_dust = dust
+		end
+
+		if sea and #sea > 0 then
+			sea = sea:split("%s", false, 1, true)
+			biome.node_water_top = sea[1]
+			biome.depth_water_top = tonumber(sea[2] or 1)
+		end
+
+		if river and #river > 0 then
+			biome.node_river_water = river
+		end
+
+		biome.y_min = tonumber(ymin) or -31000
+		biome.y_max = tonumber(ymax) or 31000
+		biome.heat_point = tonumber(heat) or 50
+		biome.humidity_point = tonumber(humidity) or 50
+
+		local id = minetest.register_biome(biome)
+		
+		print("Biome \"" .. name .. "\" registered with ID=" .. id .. ", temperature " .. biome.heat_point .. ", humidity " .. biome.humidity_point .. ".")
+	end
+end
+
+--dofile(path .. "/main_biomes.lua")
+--dofile(path .. "/beach_biomes.lua")
+--dofile(path .. "/sea_biomes.lua")
+dofile(path .. "/decorations.lua")
